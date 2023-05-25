dd112e201ba28b152b1bad0e5db9f0d5b9aba721
Kaowarstail <victor.dane@edu.devinci.fr>
2023-05-25 17:20:02 +0200

    revert migration fix

--- migrations/Version20230501210214.php
+++ migrations/Version20230501210214.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230501210052 extends AbstractMigration
+final class Version20230501210214 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20230501210052 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
+        $this->addSql('ALTER TABLE user ADD COLUMN age INTEGER NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, name FROM user');
         $this->addSql('DROP TABLE user');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
+        $this->addSql('INSERT INTO user (id, name) SELECT id, name FROM __temp__user');
+        $this->addSql('DROP TABLE __temp__user');
     }
 }
