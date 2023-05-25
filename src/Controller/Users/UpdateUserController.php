65de94ad122c5974a41a5662f787a773237ced47
cyvax <raphael.cercas@eemi.com>
2023-05-25 17:33:53 +0200

    Extracted CreateUser & UpdateUser

---
+++ src/Controller/Users/UpdateUserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller\Users;
+
+use App\Entity\User;
+use App\Forms\UserForm;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UpdateUserController extends AbstractController
+{
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {}
+
+    #[Route(
+        path: '/user/{identifiant}',
+        name: 'udpate_user',
+        methods: ['PATCH']
+    )]
+    public function updateUser(EntityManagerInterface $entityManager, $identifiant, Request $request): JsonResponse
+    {
+        $player = $this->userRepository->findOneBy(['id' => $identifiant]);
+        if (!$player) {
+            return new JsonResponse('Wrong id', 404);
+        }
+
+        $form = UserForm::buildUserForm($this->createFormBuilder())->getForm();
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+
+        if (!$form->isValid()) {
+            return new JsonResponse('Invalid form', 400);
+        }
+
+        foreach ($data as $key => $value) {
+            switch ($key) {
+                case 'nom':
+                    $user = $this->userRepository->findOneBy(['name' => $data['nom']]);
+                    if (!$user) {
+                        return new JsonResponse('Name already exists', 400);
+                    }
+
+                    $player->setName($data['nom']);
+                    $entityManager->flush();
+                    break;
+                case 'age':
+                    if ($data['age'] < User::MINIMAL_AGE) {
+                        return new JsonResponse('Wrong age', 400);
+                    }
+
+                    $player->setAge($data['age']);
+                    $entityManager->flush();
+                    break;
+            }
+        }
+
+        return new JsonResponse([
+                'name' => $player->getName(),
+                'age' => $player->getAge(),
+                'id' => $player->getId()
+            ], 200);
+    }
+}
