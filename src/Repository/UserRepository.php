767092238dfbdb833706e1de5cb0c37beb4ea869
DELAPORTE <arthur.delaporte@eemi.com>
2023-05-25 12:38:41 +0200

    Changement nom variable $entity en $entityUser dans UserRepository.php

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -21,9 +21,9 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    public function save(User $entity, bool $flush = false): void
+    public function save(User $entityUser, bool $flush = false): void
     {
-        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->persist($entityUser);
 
         if ($flush) {
             $this->getEntityManager()->flush();
