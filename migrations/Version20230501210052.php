c4c4f6396e4b640dc0f5bd8e977b38b9dba41f1b
Kaowarstail <victor.dane@edu.devinci.fr>
2023-05-25 12:29:50 +0200

    modification of user table migration (removing useless table) + indentation of game table migration file

--- migrations/Version20230501210052.php
+++ migrations/Version20230501210052.php
@@ -20,7 +20,9 @@ final class Version20230501210052 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE user (id    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                                                name VARCHAR(255) NOT NULL,
+                                                age INTEGER NOT NULL)');
     }
 
     public function down(Schema $schema): void
