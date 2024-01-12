72f232c0fc29ebbd4e772602b99cdf96558bc5d9
Marc Phrassias <marcphrassias@MacBook-Pro-de-Marc-2.local>
2024-01-12 12:36:16 +0100

    Refacto userController

---
+++ src/Controller/User/deleteUserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\User;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+
+class deleteUserController extends AbstractController {
+#[Route('/user/{id}', name: 'delete_user_by_identifiant', methods:['DELETE'])]
+    public function deleteUser($id, EntityManagerInterface $entityManager): JsonResponse | null
+    {
+        $joueur = $entityManager->getRepository(User::class)->findBy(['id'=>$id]);
+        if(count($joueur) == 1){
+            try{
+                $entityManager->remove($joueur[0]);
+                $entityManager->flush();
+
+                $existeEncore = $entityManager->getRepository(User::class)->findBy(['id'=>$id]);
+    
+                if(!empty($existeEncore)){
+                    throw new \Exception("Le user n'a pas éte délété");
+                    return null;
+                }else{
+                    return new JsonResponse('', 204);
+                }
+            }catch(\Exception $e){
+                return new JsonResponse($e->getMessage(), 500);
+            }
+        }else{
+            return new JsonResponse('Wrong id', 404);
+        }    
+    }
+}
