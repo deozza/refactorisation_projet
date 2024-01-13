c60a78398d1e9ae36ea0bf6cb41d21a24bdbb4be
Junior Djemba <135090388+Junior-Djemba@users.noreply.github.com>
2024-01-13 23:33:06 +0100

    Update UserRepository.php

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -21,22 +21,18 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    public function save(User $entity, bool $flush = false): void
+        public function save(User $entity): User
     {
         $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+        return $entity;
     }
 
-    public function remove(User $entity, bool $flush = false): void
+    public function remove(User $entity): void
     {
         $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+        $this->getEntityManager()->flush();
     }
 
 //    /**
