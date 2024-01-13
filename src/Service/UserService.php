5a801d94641ea7a66cf8877083d9b1c3a56198c5
Junior Djemba <135090388+Junior-Djemba@users.noreply.github.com>
2024-01-13 23:34:42 +0100

    Create UserService.php

---
+++ src/Service/UserService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\DTO\UserDTO;
+use App\Form\AddUserType;
+use App\Form\UpdateUserType;
+use App\Repository\UserRepository;
+
+class UserService
+{
+    private UserRepository $userRepository;
+    private FormFactoryInterface $formFactory;
+
+    public function __construct(FormFactoryInterface $formFactory, UserRepository $userRepository)
+    {
+        $this->formFactory = $formFactory;
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAllUsers(): array
+    {
+        return $this->userRepository->findAll();
+    }
+
+    public function getUser(int $id):  User | null
+    {
+        return $this->userRepository->find($id);
+    }
+
+    public function save(?User $user = null): void
+    {
+        $this->userRepository->save($user);
+    }
+
+    public function deleteUser(User $user): void
+    {
+        $this->userRepository->delete($user);
+    }
+}
